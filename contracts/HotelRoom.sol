ba9a19f9b53283a28ccc9d2b0128c2c6e8caff61
singlak999 <singlak999@outlook.com>
2024-10-07 15:04:00 +0530

    Created Hotel booking  system

---
+++ contracts/HotelRoom.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HotelRoom{
+    address payable public owner;
+    event Occupy(address occupant, uint value);
+    enum Statuses{Vacant, Occupied}
+    Statuses currentStatus;
+    constructor(){
+        owner=payable(msg.sender);
+        currentStatus=Statuses.Vacant;
+    }
+
+    modifier onlyWhileVacant{
+        require(currentStatus==Statuses.Vacant,"Currently Occupied");
+        _;
+    }
+
+    modifier costs(uint amount){
+        require(msg.value>=amount, "Not enough ether provided");
+        _;
+    }
+
+    function book() public payable onlyWhileVacant costs(2 ether){
+        require(msg.value>=2 ether, "Not enough ether provided");
+       
+        owner.transfer(msg.value);
+        (bool sent, bytes memory data)= (owner.call{value: msg.value})("");
+        currentStatus=Statuses.Occupied;
+
+        emit Occupy(msg.sender, msg.value);
+    }
+}
